877f9baba9e34ba7f8ddfb032a2ef98f9bc6e6a3
popcorntruck <57514657+popcorntruck@users.noreply.github.com>
2023-03-24 15:26:53 -0700

    it uses prisma  now

--- src/data.ts
+++ src/data.ts
@@ -1,31 +1,31 @@
-import * as mongoose from "mongoose";
+// import * as mongoose from "mongoose";
 
-// const Data = mongoose.Schema({
-//     _id: String,
-//     serverID: String,
-//     userName: String,
-//     userID: String,
-//     chair_count: String,
+// // const Data = mongoose.Schema({
+// //     _id: String,
+// //     serverID: String,
+// //     userName: String,
+// //     userID: String,
+// //     chair_count: String,
+// // });
+
+// type Profile = {
+//   userID: string;
+//   serverID: string;
+//   userName: string;
+//   serverName: string;
+//   serverPFP: string;
+//   pfp: string;
+//   chair_count: number;
+// };
+
+// const profileSchema = new mongoose.Schema({
+//   userID: { type: String, require: true },
+//   serverID: { type: String, require: true },
+//   userName: { type: String },
+//   serverName: { type: String },
+//   serverPFP: { type: String },
+//   pfp: { type: String },
+//   chair_count: { type: Number, default: 0 },
 // });
 
-type Profile = {
-  userID: string;
-  serverID: string;
-  userName: string;
-  serverName: string;
-  serverPFP: string;
-  pfp: string;
-  chair_count: number;
-};
-
-const profileSchema = new mongoose.Schema({
-  userID: { type: String, require: true },
-  serverID: { type: String, require: true },
-  userName: { type: String },
-  serverName: { type: String },
-  serverPFP: { type: String },
-  pfp: { type: String },
-  chair_count: { type: Number, default: 0 },
-});
-
-export const profileModel = mongoose.model("ProfileModels", profileSchema);
+// export const profileModel = mongoose.model("ProfileModels", profileSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import "dotenv-safe/config";
-import { Client, GatewayIntentBits, EmbedBuilder, ChatInputCommandInteraction } from "discord.js";
+import {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  ChatInputCommandInteraction,
+} from "discord.js";
 import * as fs from "fs";
-import * as mongoose from "mongoose";
-import { profileModel } from "./data";
 import { FileStore } from "./file-store";
 import { startCommands } from "./commandStarter";
+import { PrismaClient } from "@prisma/client";
 
 const bot = new Client({
   intents: [
@@ -13,8 +17,7 @@ const bot = new Client({
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
   ],
-}
-);
+});
 
 const PREFIX = "!";
 
@@ -40,14 +43,19 @@ const countStore = new FileStore<number>({
   },
 });
 
-mongoose
-  .connect(process.env.MONGOOSE_DB, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(console.error);
+// mongoose
+//   .connect(process.env.MONGOOSE_DB, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB Connected..."))
+//   .catch(console.error);
+
+const db = new PrismaClient({ log: ["error", "warn", "query", "info"] });
+db.$connect()
+  .then(() => console.log("Connected to Prisma"))
+  .catch((e) => console.error("Failed to connect to Prisma", e));
 
 bot.once("ready", async () => {
   console.log("This bot is online");
@@ -67,93 +75,145 @@ bot.on("messageCreate", async function (msg) {
 
   if ((await channelsStore.get()).includes(msg.channel.id)) {
     msg.react("🪑").catch(function (error) {
-      console.log(error)
+      console.log(error);
     });
     await countStore.save((await countStore.get()) + 1);
     bot.user.setActivity(`🪑 ${await countStore.get()} | Made by Salty Mat`);
 
     if (msg.author.bot) return;
-
-    let profileData;
     try {
-      profileData = await profileModel.findOne({
-        userID: msg.author.id,
-        serverID: msg.guild.id,
+      await db.serverProfile.upsert({
+        where: {
+          userId_serverId: { serverId: msg.guild.id, userId: msg.author.id },
+        },
+        create: {
+          chairCount: 1,
+          server: {
+            connectOrCreate: {
+              where: { id: msg.guild.id },
+              create: {
+                id: msg.guild.id,
+                name: msg.guild.name,
+                avatarUrl: msg.guild.iconURL(),
+              },
+            },
+          },
+          user: {
+            connectOrCreate: {
+              where: { id: msg.author.id },
+              create: {
+                id: msg.author.id,
+                username: msg.author.username,
+                discriminator: msg.author.discriminator,
+                avatarUrl: msg.author.avatarURL(),
+              },
+            },
+          },
+        },
+        update: { chairCount: { increment: 1 } },
       });
-      if (!profileData) {
-        let profile = await profileModel.create({
-          userID: msg.author.id,
-          serverID: msg.guild.id,
-          userName: msg.author.tag,
-          serverName: msg.guild.name,
-          serverPFP: msg.guild.iconURL(),
-          pfp: msg.author.avatarURL(),
-          chair_count: 0,
-        });
-        profile.save();
-      }
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.error("failed to update user chairs count", error);
     }
 
-    const response = await profileModel.findOneAndUpdate(
-      {
-        userID: msg.author.id,
-        serverID: msg.guild.id,
-      },
-      {
-        $set: {
-          userName: msg.author.tag,
-          serverName: msg.guild.name,
-          serverPFP: msg.guild.iconURL(),
-          pfp: msg.author.avatarURL(),
-        },
-        $inc: {
-          chair_count: 1,
-        },
-      }
-    );
+    // let profileData;
+    // try {
+    //   profileData = await profileModel.findOne({
+    //     userID: msg.author.id,
+    //     serverID: msg.guild.id,
+    //   });
+    //   if (!profileData) {
+    //     let profile = await profileModel.create({
+    //       userID: msg.author.id,
+    //       serverID: msg.guild.id,
+    //       userName: msg.author.tag,
+    //       serverName: msg.guild.name,
+    //       serverPFP: msg.guild.iconURL(),
+    //       pfp: msg.author.avatarURL(),
+    //       chair_count: 0,
+    //     });
+    //     profile.save();
+    //   }
+    // } catch (err) {
+    //   console.log(err);
+    // }
+
+    // const response = await profileModel.findOneAndUpdate(
+    //   {
+    //     userID: msg.author.id,
+    //     serverID: msg.guild.id,
+    //   },
+    //   {
+    //     $set: {
+    //       userName: msg.author.tag,
+    //       serverName: msg.guild.name,
+    //       serverPFP: msg.guild.iconURL(),
+    //       pfp: msg.author.avatarURL(),
+    //     },
+    //     $inc: {
+    //       chair_count: 1,
+    //     },
+    //   }
+    // );
   }
 });
 
 bot.on("interactionCreate", async function (msg: ChatInputCommandInteraction) {
-
   if (msg.guild === null) return;
-
 
   let msgLowercase = msg.commandName.toLowerCase();
 
-
-
   let rank = 1;
   let message: string = "";
-  let leaderbord: any;
+  let leaderbord;
 
   let profileData;
   let profileDataOther;
   try {
-    profileData = await profileModel.findOne({
-      userID: msg.user.id,
-      serverID: msg.guild.id,
+    // profileData = await profileModel.findOne({
+    //   userID: msg.user.id,
+    //   serverID: msg.guild.id,
+    // });
+    profileData = await db.serverProfile.findUnique({
+      where: {
+        userId_serverId: { userId: msg.user.id, serverId: msg.guild.id },
+      },
+      include: {
+        user: {
+          select: { avatarUrl: true },
+        },
+        server: true,
+      },
     });
   } catch (err) {
     console.log(err);
   }
   try {
-
-    profileDataOther = await profileModel.findOne({
-      userID: msg.options.data[0].user.id,
-      serverID: msg.guild.id,
+    profileDataOther = await db.serverProfile.findUnique({
+      where: {
+        userId_serverId: {
+          userId: msg.options.data[0].user.id,
+          serverId: msg.guild.id,
+        },
+      },
+      include: {
+        user: {
+          select: { avatarUrl: true },
+        },
+      },
     });
-  } catch (err) { }
+  } catch (err) {}
   if (msgLowercase == "count" && msg.options.data.length === 0) {
     const embed = new EmbedBuilder()
       .setColor("#78d6ff")
       //.setAuthor(`${msg.author.tag}`, msg.author.avatarURL())
       .setTitle(`${msg.user.username}'s chair count`)
-      .setDescription(`**__chairs__**: ${profileData.chair_count}`)
+      .setDescription(`**__chairs__**: ${profileData.chairCount}`)
       //.addField (`**${msg.author.username}'s chair count**`,`**chairs**: ${profileData.chair_count}`)
-      .setFooter({ text: "WARNING this is a beta your data might get deleted", iconURL: profileData.pfp });
+      .setFooter({
+        text: "WARNING this is a beta your data might get deleted",
+        iconURL: profileData.user.avatarUrl,
+      });
 
     msg.reply({ embeds: [embed] });
   } else if (msgLowercase == "count" && msg.options.data.length !== 0) {
@@ -162,34 +222,41 @@ bot.on("interactionCreate", async function (msg: ChatInputCommandInteraction) {
         .setColor("#78d6ff")
         //.setAuthor(`${msg.author.tag}`, msg.author.avatarURL())
         .setTitle(`${msg.options.data[0].user.username}'s chair count`)
-        .setDescription(`**__chairs__**: ${profileDataOther.chair_count}`)
+        .setDescription(`**__chairs__**: ${profileDataOther.chairCount}`)
         //.addField (`**${msg.author.username}'s chair count**`,`**chairs**: ${profileData.chair_count}`)
-        .setFooter({ text: "WARNING this is a beta your data might get deleted", iconURL: profileDataOther.pfp }
-        );
+        .setFooter({
+          text: "WARNING this is a beta your data might get deleted",
+          iconURL: profileDataOther.user.avatarUrl,
+        });
 
       msg.reply({ embeds: [embed] });
     } catch (error) {
       msg.reply("you need to ping a valid person");
     }
-  } else if (
-    msgLowercase == "leaderboard"
-  ) {
-    leaderbord = await profileModel.find({ serverID: msg.guild.id });
+  } else if (msgLowercase == "leaderboard") {
+    //leaderbord = await profileModel.find({ serverID: msg.guild.id });
+    leaderbord = await db.serverProfile.findMany({
+      where: { serverId: msg.guild.id },
+      include: {
+        user: {
+          select: { username: true },
+        },
+      },
+    });
     let embed = new EmbedBuilder()
-      .setFooter(
-        { text: `${profileData.serverName} | WARNING this is a beta your data might get deleted`, iconURL: profileData.serverPFP }
-      )
+      .setFooter({
+        text: `${profileData.server.name} | WARNING this is a beta your data might get deleted`,
+        iconURL: profileData.server.avatarUrl,
+      })
       .setColor("#78d6ff")
-      .setTitle(`${profileData.serverName}'s Chair count`);
+      .setTitle(`${profileData.server.name}'s Chair count`);
 
-    leaderbord.sort(
-      (a: { chair_count: number }, b: { chair_count: number }) =>
-        b.chair_count - a.chair_count
-    );
+    leaderbord.sort((a, b) => b.chairCount - a.chairCount);
 
-    leaderbord.forEach((e: { userName: any; chair_count: any }) => {
-      message += `\n**${rank}** | **${e.userName.slice(0, -5)}** 🪑: ${e.chair_count
-        }`;
+    leaderbord.forEach((e) => {
+      message += `\n**${rank}** | **${e.user.username.slice(0, -5)}** 🪑: ${
+        e.chairCount
+      }`;
       rank++;
     });
     embed.setDescription(message);
@@ -197,28 +264,36 @@ bot.on("interactionCreate", async function (msg: ChatInputCommandInteraction) {
     rank = 1;
     message = "";
   } else if (msgLowercase == "add") {
-
     const currentChannels = await channelsStore.get();
     await channelsStore.save([...currentChannels, msg.channel.id]);
     msg.reply("channel added ✅ ᶦ ᵗʰᶦⁿᵏ");
-
   } else if (msgLowercase == "remove") {
-
     const currentChannels = await channelsStore.get();
     const newChans = currentChannels.filter((val) => msg.channel.id !== val);
     await channelsStore.save(newChans);
     msg.reply("channel removed ✅ ᶦ ᵗʰᶦⁿᵏ");
-
   } else if (msgLowercase == "developer") {
     msg.reply("https://imgur.com/a/CGOkqSE");
   } else if (msgLowercase == "suggest") {
     const user = bot.users.cache.get("173569047684841472");
-    user.send("***from:*** " + msg.user.username + "#" + msg.user.discriminator + "\n```" + msg.options.data[0].value + "```");
+    user.send(
+      "***from:*** " +
+        msg.user.username +
+        "#" +
+        msg.user.discriminator +
+        "\n```" +
+        msg.options.data[0].value +
+        "```"
+    );
     msg.reply("suggestion sent ✅ ᶦ ᵗʰᶦⁿᵏ");
-
   } else if (msgLowercase == "debug") {
     const currentChannels = await channelsStore.get();
-    msg.reply("```" +currentChannels + "```" + `🏓 API Latency is ${Math.round(bot.ws.ping)}ms`);
+    msg.reply(
+      "```" +
+        currentChannels +
+        "```" +
+        `🏓 API Latency is ${Math.round(bot.ws.ping)}ms`
+    );
   } else if (msgLowercase == "!commands" || msgLowercase == "!command") {
     msg.reply(
       "**!add\n!remove\n!about\n!suggest\n!uptime\n**i will make it look better later afjlafkkfds :char:"
@@ -231,18 +306,14 @@ bot.on("interactionCreate", async function (msg: ChatInputCommandInteraction) {
     let minutes = Math.floor(bot.uptime / 60000) % 60;
     let seconds = Math.floor(bot.uptime / 1000) % 60;
 
-    const embed = new EmbedBuilder()
-      .setColor("#78d6ff")
-      .addFields({
-        name: "**__uptime__**",
-        value: `**days**: ${days} \n**hours**: ${hours} \n**minutes**: ${minutes} \n**seconds**: ${seconds}`
-      }
-      );
+    const embed = new EmbedBuilder().setColor("#78d6ff").addFields({
+      name: "**__uptime__**",
+      value: `**days**: ${days} \n**hours**: ${hours} \n**minutes**: ${minutes} \n**seconds**: ${seconds}`,
+    });
 
     msg.reply({ embeds: [embed] });
   }
 });
-
 
 const aboutEmbed = new EmbedBuilder()
   .setColor("#78d6ff")
@@ -252,15 +323,17 @@ const aboutEmbed = new EmbedBuilder()
   .setAuthor({
     name: "Made by: Salty Mat",
     iconURL: "https://imgur.com/PWR3DeX.png",
-    url: "https://twitter.com/Salty_Mat"
-  }
-  )
+    url: "https://twitter.com/Salty_Mat",
+  })
   .setTitle("About")
   .setThumbnail("https://imgur.com/ablIrp4.png")
   .addFields(
     { name: "Discord server", value: "coming soon" },
     { name: "Email", value: "chairbot.discord@gmail.com" },
-    { name: `contact me`, value: `**twitter**: @Salty_Mat\n**Youtube**: Salty Mat\n**Discord**: Salt#0330 ` }
+    {
+      name: `contact me`,
+      value: `**twitter**: @Salty_Mat\n**Youtube**: Salty Mat\n**Discord**: Salt#0330 `,
+    }
   )
   .setFooter({ text: "Chair bot was born at jun 10th 2020" });
 
